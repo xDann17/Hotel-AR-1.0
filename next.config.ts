d9f26ab647e2edc240ebba5d089397a22553c295
Daniel Baquero <daniel.rri194bo@gmail.com>
2025-10-19 20:51:00 -0400

    chore: temporarily ignore lint/ts errors on build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+esLint: {
+  ignoreDuringBuilds: true,
+},
+typescript: {
+  ignoreBuildErrors: true,
+},
 };
+
 
 export default nextConfig;
