bba6014ab61ca30a1621f789377e5c46628fc773
Daniel Baquero <daniel.rri194bo@gmail.com>
2025-10-19 21:36:39 -0400

    chore: fixing second problem from projects

--- src/app/projects/expenses/new/page.tsx
+++ src/app/projects/expenses/new/page.tsx
@@ -1,8 +1,5 @@
 'use client'
 
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-
 import { useEffect, useMemo, useState } from 'react'
 import Link from 'next/link'
 import { useRouter, useSearchParams } from 'next/navigation'
